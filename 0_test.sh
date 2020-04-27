a42dd69fbd760b6d816fc00ab888bbd48d7cf542
chengfengpiaopiao <869010486@qq.com>
2020-04-28 07:52:18 +0800

    基本操作

--- 0_test.sh
+++ 0_test.sh
@@ -22,4 +22,31 @@ fi
 path=`pwd`
 echo $path
 
+###统计目录下共有多少个文件并显示
+
+cd /f/Tool/Vagrant/pro-192.168.222.111/data/sh
+
+for index in /f/Tool/Vagrant/pro-192.168.222.111/data/sh/*
+do
+	echo "hello , $index"
+done
+count=`ls -l|grep '^-'|wc -l`
+echo "----文件总数为$count"
+
+
+###外部传参给脚本使用
+
+number1=$1
+number2=$2
+[ -z $number1 ] && echo "please \$number1 number1" && exit 1
+[ -z $number2 ] && echo "please \$number2 number2" && exit 1
+[ ! $# -eq 2 ] && echo "--please num1 num2" && exit 3
+echo "传参$number1 + 传参$number2 = $(($number1+$number2))"
+
+
+
+
+
+
+
 
